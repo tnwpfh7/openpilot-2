c47ff0a2dce20ede877b87e6b1501ada9ee92f6d
SangJo Kim <45070398+KimSangJo@users.noreply.github.com>
2021-04-06 15:21:25 +0900

    change access permition

--- panda/crypto/sign.py
+++ panda/crypto/sign.py
@@ -6,7 +6,7 @@ import hashlib
 from Crypto.PublicKey import RSA
 import binascii
 
-# increment this to make new hardware not run old versions
+# increase this to make new hardware not run old versions
 VERSION = 2
 
 rsa = RSA.importKey(open(sys.argv[3]).read())
