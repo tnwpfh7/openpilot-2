d58db5944fc827a943386b017e0365cb20fb9173
SangJo Kim <45070398+KimSangJo@users.noreply.github.com>
2021-04-28 22:04:34 +0900

    Add Kegman's Lateral&Long tuner

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -1,7 +1,9 @@
 from cereal import log
 from common.numpy_fast import clip, interp
 from selfdrive.controls.lib.pid import PIController
+from selfdrive.kegman_kans_conf import kegman_kans_conf
 
+kegman_kans = kegman_kans_conf()
 LongCtrlState = log.ControlsState.LongControlState
 
 STOPPING_EGO_SPEED = 0.5
@@ -9,7 +11,9 @@ STOPPING_TARGET_SPEED_OFFSET = 0.01
 STARTING_TARGET_SPEED = 0.5
 BRAKE_THRESHOLD_TO_PID = 0.2
 
-BRAKE_STOPPING_TARGET = 0.5  # apply at least this amount of brake to maintain the vehicle stationary
+STOPPING_BRAKE_RATE = 0.2  # brake_travel/s while trying to stop
+STARTING_BRAKE_RATE = 0.8  # brake_travel/s while releasing on restart
+BRAKE_STOPPING_TARGET = float(kegman_kans.conf['brakeStoppingTarget'])  # apply at least this amount of brake to maintain the vehicle stationary
 
 RATE = 100.0
 
